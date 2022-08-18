dd278e2ed6badf618e090d38b4cb453ca887022f
Ramon Soares <ramongsoares@gmail.com>
2022-08-18 16:08:02 -0300

    add vueuse and schemaOrg modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,10 +5,15 @@ export default defineNuxtConfig({
     modules: [
         '@nuxtjs/tailwindcss', // https://tailwindcss.nuxtjs.org/
         '@nuxt/content', // https://content.nuxtjs.org/
-        '@nuxt/image-edge', // https://v1.image.nuxtjs.org/
+        // '@nuxt/image-edge', // https://v1.image.nuxtjs.org/
         '@pinia/nuxt', // https://pinia.vuejs.org/
+        '@vueuse/nuxt', // https://vueuse.org/
+        'nuxt-schema-org', // https://vue-schema-org.netlify.app/
     ],
     content: {},
+    schemaOrg: {
+        canonicalHost: 'https://localhost:3000/'
+    },
     typescript: {
         strict: true
     }
