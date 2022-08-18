f475a26de2706bd89abda6865c5dc5f6e60da10f
Ramon Soares <ramongsoares@gmail.com>
2022-08-18 15:41:46 -0300

    add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,8 @@ export default defineNuxtConfig({
     modules: [
         '@nuxtjs/tailwindcss', // https://tailwindcss.nuxtjs.org/
         '@nuxt/content', // https://content.nuxtjs.org/
-        '@nuxt/image-edge' // https://v1.image.nuxtjs.org/
+        '@nuxt/image-edge', // https://v1.image.nuxtjs.org/
+        '@pinia/nuxt', // https://pinia.vuejs.org/
     ],
     content: {},
     typescript: {
