5d1cdc7adf44fc385734fbac9142a4a7e1850ec3
Ramon Soares <ramongsoares@gmail.com>
2022-08-22 15:08:49 -0300

    add vscode launch config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,20 +2,20 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/tailwindcss', // https://tailwindcss.nuxtjs.org/
-        '@nuxt/content', // https://content.nuxtjs.org/
-        // '@nuxt/image-edge', // https://v1.image.nuxtjs.org/
-        '@pinia/nuxt', // https://pinia.vuejs.org/
-        '@vueuse/nuxt', // https://vueuse.org/
-        'nuxt-schema-org', // https://vue-schema-org.netlify.app/
-        'nuxt-xstate', // https://nuxt-xstate.lexpeartha.com/
-    ],
-    content: {},
-    schemaOrg: {
-        canonicalHost: 'https://localhost:3000/'
-    },
-    typescript: {
-        strict: true
-    }
+  modules: [
+    '@nuxtjs/tailwindcss', // https://tailwindcss.nuxtjs.org/
+    '@nuxt/content', // https://content.nuxtjs.org/
+    // '@nuxt/image-edge', // https://v1.image.nuxtjs.org/
+    '@pinia/nuxt', // https://pinia.vuejs.org/
+    '@vueuse/nuxt', // https://vueuse.org/
+    'nuxt-schema-org', // https://vue-schema-org.netlify.app/
+    'nuxt-xstate' // https://nuxt-xstate.lexpeartha.com/
+  ],
+  content: {},
+  schemaOrg: {
+    canonicalHost: 'https://localhost:3000/'
+  },
+  typescript: {
+    strict: true
+  }
 })
